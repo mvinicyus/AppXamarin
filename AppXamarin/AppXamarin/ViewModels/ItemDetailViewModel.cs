9f3f12ed8880e6366e864088ddf8659459d259e5
Markus Vinicyus <markusvinicyus@live.com>
2019-03-09 19:10:23 -0300

    primeiro commit

---
+++ AppXamarin/AppXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppXamarin.Models;
+
+namespace AppXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
